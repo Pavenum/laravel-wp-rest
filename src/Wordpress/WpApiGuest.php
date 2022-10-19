7344e1321a7b2b878ee5939cb8b75a1eda00bda5
DoudouMoii <etienne@soumettre.fr>
2022-10-19 15:43:51 +0200

    throw exception

--- src/Wordpress/WpApiGuest.php
+++ src/Wordpress/WpApiGuest.php
@@ -2,11 +2,17 @@
 
 namespace Pavenum\LaravelWpRest\Wordpress;
 
+use Exception;
 use GuzzleHttp\Psr7\Header;
+use Pavenum\LaravelWpRest\Exceptions\WordpressRequestException;
 use Throwable;
 
 class WpApiGuest extends WpApiBaseRequest
 {
+    /**
+     * @throws WordpressRequestException
+     * @throws Exception
+     */
     public function findApiBaseUrl(string $website_home_url): string|false
     {
         $response = $this->makeRequest(endpoint: $website_home_url, method: 'HEAD');
@@ -30,16 +36,20 @@ class WpApiGuest extends WpApiBaseRequest
             }
         }
 
-        return false;
+        throw new Exception('Could not find the base API URL', 404);
     }
 
+    /**
+     * @throws WordpressRequestException
+     * @throws Exception
+     */
     public function findBaseAppPasswordsAuthUrl(string $base_api_url): string|false
     {
         $response = $this->makeRequest(endpoint:  $base_api_url);
         $app_passwords = object_get($response, 'authentication.application-passwords.endpoints.authorization', false);
 
         if (!$app_passwords) {
-            throw new \Exception('Application Passwords not enabled on this website', 400);
+            throw new Exception('Application Passwords not enabled on this website', 400);
         }
 
         return $app_passwords;
@@ -54,8 +64,8 @@ class WpApiGuest extends WpApiBaseRequest
         $scheme_success_url = parse_url($success_url, PHP_URL_SCHEME);
         $scheme_reject_url = parse_url($reject_url, PHP_URL_SCHEME);
 
-        throw_unless($scheme_success_url === 'https', \Exception::class, "Success url have to be secured (https)", 400);
-        throw_unless($scheme_reject_url === 'https', \Exception::class, "Reject url have to be secured (https)", 400);
+        throw_unless($scheme_success_url === 'https', Exception::class, "Success url have to be secured (https)", 400);
+        throw_unless($scheme_reject_url === 'https', Exception::class, "Reject url have to be secured (https)", 400);
 
         $query = http_build_query([
             'app_name' => $app_name,
@@ -65,10 +75,6 @@ class WpApiGuest extends WpApiBaseRequest
         ]);
 
         $auth_app_url = $this->findBaseAppPasswordsAuthUrl($base_api_url);
-
-        if (!$auth_app_url) {
-            return $auth_app_url;
-        }
 
         return $auth_app_url . '?' . $query;
     }
